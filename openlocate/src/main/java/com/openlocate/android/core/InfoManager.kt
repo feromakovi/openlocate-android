1c10bd1373d5db200a97cc27ce918773307c987f
Richard Samela <feromakovi@gmail.com>
2018-09-17 09:31:29 +0200

    Refactor InfoManager implementation

--- openlocate/src/main/java/com/openlocate/android/core/InfoManager.kt
+++ openlocate/src/main/java/com/openlocate/android/core/InfoManager.kt
@@ -1,5 +1,6 @@
 package com.openlocate.android.core
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_NONE
@@ -8,51 +9,47 @@ import android.content.Context.NOTIFICATION_SERVICE
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.support.v4.app.NotificationCompat
+import com.openlocate.android.core.InfoManager.Companion.CHANNEL_ID
 import com.openlocate.android.core.InfoManager.Companion.NOTIFICATION_ID
 
 /**
  * Created by feromakovi <feromakovi@gmail.com> on 31/08/2018.
  */
-class InfoManager @JvmOverloads constructor(val context : Context, private val debug : Boolean = false) {
+class InfoManager @JvmOverloads constructor(private val context : Context, private val debug : Boolean = false) {
 
   private val locationsDB : LocationDatabase = LocationDatabase(DatabaseHelper.getInstance(context))
   private val storedLocations: Long = locationsDB.size()
   private val notificationManager : NotificationManager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 
-  fun store(count: Int) {
-    if (debug) {
-      val allStored = locationsDB.size()
-      val text = "DB items: $storedLocations - Inserted: $count - All: $allStored"
-      notificationManager.show(context, text)
-    }
-  }
+  fun store(count: Int) = count.takeIf { debug }?.let { createInfo("Stored: $it") }
 
-  fun sync(count: Int) {
-    if (debug) {
-      val allStored = locationsDB.size()
-      val text = "DB items: $storedLocations - Synced: $count - All: $allStored"
-      notificationManager.show(context, text)
-    }
-  }
+  fun sync(count: Int) = count.takeIf { debug }?.let { createInfo("Synced: $it") }
 
-  companion object {
-    internal const val NOTIFICATION_ID = 123
-  }
-}
+  private fun createInfo(action: String) {
+    val allStored = locationsDB.size()
+    val text = "DB items: $storedLocations - $action - All: $allStored"
 
-private fun NotificationManager.show(context: Context, text : String) {
-  val channelId = "open-locate"
-  if (VERSION.SDK_INT >= VERSION_CODES.O) {
-    val channel = NotificationChannel(channelId, "open locate service", IMPORTANCE_NONE)
-    createNotificationChannel(channel)
-  }
-
-  val notification = NotificationCompat.Builder(context, channelId)
+    val notification = NotificationCompat.Builder(context, CHANNEL_ID)
       .setSmallIcon(android.R.drawable.ic_menu_mylocation)
       .setContentTitle("Locations")
       .setContentText(text)
       .setPriority(NotificationCompat.PRIORITY_DEFAULT)
       .build()
+
+    notificationManager.show(notification)
+  }
+
+  companion object {
+    internal const val NOTIFICATION_ID = 123
+    internal const val CHANNEL_ID = "open-locate"
+  }
+}
+
+private fun NotificationManager.show(notification: Notification) {
+  if (VERSION.SDK_INT >= VERSION_CODES.O) {
+    val channel = NotificationChannel(CHANNEL_ID, "open locate service", IMPORTANCE_NONE)
+    createNotificationChannel(channel)
+  }
 
   notify(NOTIFICATION_ID, notification)
 }
